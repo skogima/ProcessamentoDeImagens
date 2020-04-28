df1348d072919246bfdadc3518173a654cb86f5e
skogima <50327059+skogima@users.noreply.github.com>
2020-04-28 11:53:33 -0300

    Adicionado histograma

---
+++ ProjetoFinal/Efeitos/Histograma.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace ProjetoFinal
+{
+    public class Histograma
+    {
+        private enum Canal
+        {
+            Vermelho,
+            Verde,
+            Azul,
+            Cinza
+        }
+        private static Dictionary<int, int> GerarHistograma(Bitmap bitmap, Canal canal)
+        {
+            var histograma = new Dictionary<int, int>();
+            for (int i = 0; i < 256; i++)
+            {
+                histograma.Add(i, 0);
+            }
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    Color c = bitmap.GetPixel(i, j);
+
+                    switch (canal)
+                    {
+                        case Canal.Vermelho:
+                            histograma[c.R] += 1;
+                            break;
+                        case Canal.Verde:
+                            histograma[c.G]++;
+                            break;
+                        case Canal.Azul:
+                            histograma[c.B]++;
+                            break;
+                        case Canal.Cinza:
+                            histograma[c.R]++;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            return histograma;
+        }
+
+        public static Dictionary<int, int> GerarHistogramaVermelho(Bitmap bitmap)
+        {
+            return GerarHistograma(bitmap, Canal.Vermelho);
+        }
+
+        public static Dictionary<int, int> GerarHistogramaAzul(Bitmap bitmap)
+        {
+            return GerarHistograma(bitmap, Canal.Azul);
+        }
+
+        public static Dictionary<int, int> GerarHistogramaVerde(Bitmap bitmap)
+        {
+            return GerarHistograma(bitmap, Canal.Verde);
+        }
+        public static Dictionary<int, int> GerarHistogramaCinza(Bitmap bitmap)
+        {
+            return GerarHistograma(bitmap, Canal.Cinza);
+        }
+
+    }
+}
---
+++ ProjetoFinal/Helpers/HistogramaHelper.cs
@@ -0,0 +1,51 @@
+﻿using OxyPlot;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace ProjetoFinal
+{
+    public class HistogramaHelper : BaseViewModel
+    {
+        public List<DataPoint> HistogramaR { get; set; }
+        public List<DataPoint> HistogramaG { get; set; }
+        public List<DataPoint> HistogramaB { get; set; }
+        public HistogramaHelper()
+        {
+            HistogramaR = new List<DataPoint>();
+            HistogramaG = new List<DataPoint>();
+            HistogramaB = new List<DataPoint>();
+        }
+
+        public bool EstaVazio()
+        {
+            if (HistogramaR.Count > 0 || HistogramaG.Count > 0 || HistogramaB.Count > 0)
+                return false;
+            return true;
+        }
+
+        public void Limpar()
+        {
+            HistogramaB.Clear();
+            HistogramaG.Clear();
+            HistogramaR.Clear();
+        }
+
+        public void Calcular(Bitmap bitmap)
+        {
+            HistogramaR.Clear();
+            HistogramaG.Clear();
+            HistogramaB.Clear();
+
+            var db = Histograma.GerarHistogramaAzul(bitmap);
+            var dr = Histograma.GerarHistogramaVermelho(bitmap);
+            var dg = Histograma.GerarHistogramaVerde(bitmap);
+
+            for (int i = 0; i < db.Count; i++)
+            {
+                HistogramaR.Add(new DataPoint(i, dr[i]));
+                HistogramaG.Add(new DataPoint(i, dg[i]));
+                HistogramaB.Add(new DataPoint(i, db[i]));
+            }
+        }
+    }
+}
